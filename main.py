1ef74bcb2197064acee03c5446dc2fd82fb0e7cf
atom-man59 <aboubakr.attof@gmail.com>
2025-01-11 19:59:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from sys import exit
 from random import randint, shuffle, uniform
 from math import sqrt, cos, sin, radians
 
-bold = "\033[1m{}\033[0m"
-red = "\033[0;31m{}\033[0m"
-green = "\033[0;32m{}\033[0m"
-blue = "\033[0;34m{}\033[0m"
-cyan = "\033[0;36m{}\033[0m"
+BOLD = "\033[1m{}\033[0m"
+RED = "\033[0;31m{}\033[0m"
+GREEN = "\033[0;32m{}\033[0m"
+BLUE = "\033[0;34m{}\033[0m"
+CYAN = "\033[0;36m{}\033[0m"
 
 symbols = {
     '#': "██",
-    's': green.format("██"),
-    'S': blue.format("██"),
-    'F': red.format("██"),
+    's': GREEN.format("██"),
+    'S': BLUE.format("██"),
+    'F': RED.format("██"),
     ' ': "  "
 }
 
@@ -20,7 +20,100 @@ patterns = ((-1, 0), (1, 0), (0, -1), (0, 1))
 
 
 def print_error(text):
-    print(red.format("error: "+text))
+    print(RED.format("error: "+text))
+
+
+class MazeManager():
+    def __init__(self):
+        pass
+
+    def generate_walls_from_center(self, maze, center, radius,
+                                   num_walls, noise_level):
+        """
+        Generate walls in the labyrinth
+
+        :param maze: Maze class instance
+        :param center: coordinates of the maze center
+        :param radius: radius from which the walls will be generated
+        :param num_walls: number of walls to generate
+        :param noise_level: noise to add
+        """
+
+        for i in range(num_walls):
+            angle = radians(i * (360 / num_walls))
+            x = int(center[0] + radius * cos(angle) + uniform(-noise_level,
+                                                              noise_level))
+            y = int(center[1] + radius * sin(angle) + uniform(-noise_level,
+                                                              noise_level))
+
+            if maze.cell_valid((x, y)):
+                maze.grid[y][x] = '#'
+
+    def generate_maze(self, width: int, height: int):
+        """
+        :param width: width of the maze grid
+        :param height: height of the maze grid
+        Returns a Maze object with walls, start and finish of the specified
+        dimensions
+        """
+        grid = [[' ' for _ in range(width)] for _ in range(height)]
+        start, finish = (0, 0), (width-1, height-1)
+        grid = [[' ' for _ in range(width)] for _ in range(height)]
+        m = Maze(grid)
+        case = start
+        forbidden_cells = [start]
+        path = []
+        diago = width**2+height**2
+        while case != finish:
+            cells = m.get_patterns(case)
+            for c in cells:
+                for fc in forbidden_cells:
+                    if fc in cells:
+                        cells.remove(fc)
+                for pc in path:
+                    if pc in cells:
+                        cells.remove(pc)
+            if len(cells) == 0:
+                case = start
+                forbidden_cells = [start]
+                path = [start]
+                continue
+            proba = []
+            for c in cells:
+                dist = sqrt((c[0] - finish[0])**2+(c[1] - finish[1])**2)+1
+                proba += [c for _ in range(int(diago/dist)**2)]
+            shuffle(proba)
+            forbidden_cells += cells
+            path.append(proba[0])
+            case = proba[0]
+
+        m.start, m.finish = start, finish
+        m.grid[start[1]][start[0]] = 'S'
+        m.grid[finish[1]][finish[0]] = 'F'
+        path.append(start)
+        path.append(finish)
+
+        for y in range(height):
+            for _ in range(randint(2*width//3, width-1)):
+                x = randint(0, width-1)
+                if (x, y) not in path:
+                    m.grid[y][x] = '#'
+
+        return m
+
+    def generate_maze_with_cosinus(self, width: int, height: int):
+        grid = [[' ' for _ in range(width)] for _ in range(height)]
+        start, finish = (0, 0), (width-1, height-1)
+        center = (width // 2, height // 2)
+        radius = min(width, height) // 3
+        num_walls = width*height // 3
+        noise_level = 5
+
+        m = Maze(grid)
+        self.generate_walls_from_center(m, center, radius,
+                                        num_walls, noise_level)
+        m.set_start_finish(start, finish)
+        return m
 
 
 class Maze:
@@ -85,92 +178,6 @@ class Maze:
         return False
 
 
-def generate_maze(width: int, height: int):
-    """
-    :param width: width of the maze grid
-    :param height: height of the maze grid
-    Returns a Maze object with walls, start and finish of the specified dimensions
-    """
-    grid = [[' ' for _ in range(width)] for _ in range(height)]
-    start, finish = (0, 0), (width-1, height-1)
-    grid = [[' ' for _ in range(width)] for _ in range(height)]
-    m = Maze(grid)
-    case = start
-    forbidden_cells = [start]
-    path = []
-    diago = width**2+height**2
-    while case != finish:
-        cells = m.get_patterns(case)
-        for c in cells:
-            for fc in forbidden_cells:
-                if fc in cells:
-                    cells.remove(fc)
-            for pc in path:
-                if pc in cells:
-                    cells.remove(pc)
-        if len(cells) == 0:
-            case = start
-            forbidden_cells = [start]
-            path = [start]
-            continue
-        proba = []
-        for c in cells:
-            dist = sqrt((c[0] - finish[0])**2+(c[1] - finish[1])**2)+1
-            proba += [c for _ in range(int(diago/dist)**2)]
-        shuffle(proba)
-        forbidden_cells += cells
-        path.append(proba[0])
-        case = proba[0]
-
-    m.start, m.finish = start, finish
-    m.grid[start[1]][start[0]] = 'S'
-    m.grid[finish[1]][finish[0]] = 'F'
-    path.append(start)
-    path.append(finish)
-
-    for y in range(height):
-        for _ in range(randint(2*width//3, width-1)):
-            x = randint(0, width-1)
-            if (x, y) not in path:
-                m.grid[y][x] = '#'
-
-    return m
-
-
-def generate_walls_from_center(maze, center, radius, num_walls, noise_level):
-    """
-    Génère des murs dans le labyrinthe à partir du centre en utilisant les fonctions cosinus et sinus avec du bruit.
-
-    :param maze: L'instance de la classe Maze.
-    :param center: Les coordonnées du centre du labyrinthe (x, y).
-    :param radius: Le rayon à partir duquel les murs seront générés.
-    :param num_walls: Le nombre de murs à générer.
-    :param noise_level: Le niveau de bruit à ajouter aux positions des murs.
-    """
-    for i in range(num_walls):
-        angle = radians(i * (360 / num_walls))
-        x = int(center[0] + radius * cos(angle) + uniform(-noise_level, noise_level))
-        y = int(center[1] + radius * sin(angle) + uniform(-noise_level, noise_level))
-
-        if maze.cell_valid((x, y)):
-            maze.grid[y][x] = '#'
-
-
-def generate_maze_with_cosinus(width: int, height: int):
-    grid = [[' ' for _ in range(width)] for _ in range(height)]
-    start, finish = (0, 0), (width-1, height-1)
-    center = (width // 2, height // 2)
-    radius = min(width, height) // 3
-    num_walls = width*height // 3
-    noise_level = 5
-
-    m = Maze(grid)
-    generate_walls_from_center(m, center, radius, num_walls, noise_level)
-
-    m.set_start_finish(start, finish)
-    return m
-
-
 def get_input(input_func, retry_text=None):
     while True:
         try:
@@ -180,14 +187,14 @@ def get_input(input_func, retry_text=None):
                 return value
 
             if retry_text:
-                print(blue.format(retry_text))
+                print(BLUE.format(retry_text))
 
         except KeyboardInterrupt:
             print("\nInterruption")
             exit(0)
 
 
-def input_user_maze():
+def input_user_maze(manager: MazeManager):
     try:
         height, width = map(int,
                             input("enter maze dimensions (height width): ")
@@ -212,23 +219,25 @@ def input_user_maze():
 
 
 def main():
-    title = bold.format("Maze solver")
-    print(green.format("┌─────────────┐"))
-    print(green.format('│'), title,  green.format('│'))
-    print(green.format("└─────────────┘"))
+    title = BOLD.format("Maze solver")
+    print(GREEN.format("┌─────────────┐"))
+    print(GREEN.format('│'), title,  GREEN.format('│'))
+    print(GREEN.format("└─────────────┘"))
 
-    print("by", cyan.format("atom-man59"), "and", cyan.format("freshkii"),
+    print("by", CYAN.format("atom-man59"), "and", CYAN.format("freshkii"),
           "\n\n")
 
+    manager = MazeManager()
+
     if input("type anything if you want to pass in a maze else a maze will be randomly generated: "):
-        maze = get_input(input_user_maze)
+        maze = get_input(lambda: input_user_maze(manager))
         start, finish = map(lambda x: tuple(map(int, x.split(","))),
                             get_input(lambda: input("enter start and finish cell (xs,ys xf,yf): "))
                             .split())
         maze.set_start_finish(start, finish)
         maze.print()
     else:
-        maze = generate_maze_with_cosinus(20, 20)
+        maze = manager.generate_maze_with_cosinus(20, 20)
         maze.print()
 
     if maze.solve():
